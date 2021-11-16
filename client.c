c14f4644e16c8318e89fa6679b225d40c87a59d3
jgmartin <jgmartin@co2061-08.ece.iastate.edu>
2021-11-16 15:35:50 -0600

    added socket creation to client.c, and server.c

--- client.c
+++ client.c
@@ -4,9 +4,33 @@
 #include <sys/types.h>
 #include <pthread.h>
 
+struct sockaddr_in DSS_Addr, DataSub1, DataSub2, DataSub3;
+int controlfd, child1, child2, child3;
+int control_port = 9082;
 
 int main()
 {
+	//form control socket 
+	if ( (controlfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) 
+		{
+        		perror("socket creation failed");
+        		exit(EXIT_FAILURE);
+    		}
+
+	//form address
+	DSS_Addr.sin_family = AF_INET;
+	DSS_Addr.sin_port = htons(control_port);
+	DSS_Addr.sin_addr.s_addr = inet_addr(serv_IP);
+
+	//establish "control" connection
+	if (connect(controlfd, (struct sockaddr *) &DSS_Addr, sizeof(DSS_Addr)) != 0) 
+	{
+		perror("connection with server failed");
+		exit(EXIT_FAILURE);	
+	}
+
+	//establish and fork 3 connections so each process is a TCP subflow
+
 	int conn1, conn2, conn3;
 	pid_t parent, id1, id2, id3;
 	
